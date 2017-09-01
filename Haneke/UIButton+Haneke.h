4ef1b9a6e1cad41039eef431825d4d51cf4a6500
Devin Ross <me@devinross.com>
2017-09-01 18:06:42 -0400

    Reduce compile warnings.

--- Haneke/UIButton+Haneke.h
+++ Haneke/UIButton+Haneke.h
@@ -41,7 +41,7 @@
 - (void)hnk_setImageFromURL:(NSURL*)url forState:(UIControlState)state placeholder:(UIImage*)placeholder;
 
 /** Loads, resizes, displays and caches an appropiately sized foreground image from the given url. If a success block is provided you will be responsible for setting the image.
- @param url URL from which the image will be loaded if it's not available in the cache.
+ @param URL URL from which the image will be loaded if it's not available in the cache.
  @param state Control state that will use the image.
  @param placeholder Image to be used as a placeholder until the requested image is ready. The placeholder image will only be used if the requested image is not available in the memory cache. If nil, the button will not change its image until the requested image is ready.
  @param successBlock Block to be called when the requested image is ready to be set. If provided, the block is reponsible for setting the image. Can be nil.
